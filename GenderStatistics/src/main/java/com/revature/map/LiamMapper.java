e04c83db69f85955f164aaa5be749e0d7e24b985
LiamCahill <cahill.liam220@gmail.com>
2018-12-10 23:39:43 -0500

    Push 1 with Mapper, Reducer, Driver

---
+++ GenderStatistics/src/main/java/com/revature/map/LiamMapper.java
@@ -0,0 +1,43 @@
+package com.revature.map;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+class LiamMapper extends Mapper<Text, Text, Text, Text> {
+
+    /*
+     * The map method runs once for each line of text in the input file. The method
+     * receives a key of type LongWritable, a value of type Text, and a Context
+     * object.
+     */
+    @Override
+    public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+
+        /*
+         * Convert the line, which is received as a Text object, to a String object.
+         */
+        String line = value.toString();
+
+        /*
+         * The line.split("\\W+") call uses regular expressions to split the line up by
+         * non-word characters.
+         * 
+         * If you are not familiar with the use of regular expressions in Java code,
+         * search the web for "Java Regex Tutorial."
+         */
+        for (String word : line.split("\\W+")) {
+            if (word.length() > 0) {
+
+                /*
+                 * Call the write method on the Context object to emit a key and a value from
+                 * the map method.
+                 */
+                context.write(new Text(word), new IntWritable(1));
+            }
+        }
+    }
+
+}
---
+++ GenderStatistics/src/main/java/com/revature/reducer/LiamReducer.java
@@ -0,0 +1,15 @@
+package com.revature.reducer;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+class LiamReducer extends Reducer<Text, Text, Text, Text> {
+
+    public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
+            throws IOException, InterruptedException {
+
+    }
+}
